2fac6a75b59e4d1cea3642573285ca8de8cf0b57
Elvis Nazifi <elvis.nazifi94@gmail.com>
2019-05-02 20:49:45 +0200

    Pass params like q string to build us burger in checkout view

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -10,10 +10,33 @@ class Checkout extends Component {
             cheese: 1
         }
     }
+
+    componentDidMount () {
+        const querry = new URLSearchParams(this.props.location.search);
+        const ingredients = {};
+
+        for (let param of querry.entries()) {
+            ingredients[param[0]] = + param[1];
+        }
+
+        this.setState({ingredients:ingredients});
+    }
+
+    checkoutCancelledHandler = () => {
+        this.props.history.goBack();
+    }
+
+    checkoutContinueHandler = () => {
+        this.props.history.replace('/checkout/contact-data');
+    }
+
     render() {
         return (
             <div>
-                <CheckoutSummary ingredients={this.state.ingredients} />
+                <CheckoutSummary 
+                ingredients={this.state.ingredients}
+                checkoutContinued = {this.checkoutContinueHandler}
+                onChekoutCancel = {this.checkoutCancelledHandler} />
             </div>
         );
     }
