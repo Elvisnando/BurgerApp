290686c007701aa42366e4143ee63ae8e0503475
Elvis Nazifi <elvis.nazifi94@gmail.com>
2019-05-18 22:01:26 +0200

    Add in the reduces logic to increase and decrease ingredient

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,12 +1,43 @@
 import * as actionType from './actions';
 
 const initialState = {
-    ingredients: null,
+    ingredients: {
+        salad: 0,
+        bacon: 0,
+        cheese: 0,
+        meat: 0
+    },
     totalPrice: 2,
 
 };
 
 const reducer = (state = initialState, action) => {
+
+    switch (action.type) {
+        case actionType.ADD_INGREDIENT:
+            return {
+                ...state, // only so we don't creat a deep clone of the object
+                ingredients: {
+                    ...state.ingredients, //for that reason i need another copy
+                    //I use [] Es6 for selec and ovveraid a proprety in this case a ingredit
+                    [action.ingredientName]: state.ingredients[action.ingredientname] + 1
+                }
+            };
+        case actionType.REMOVE_INGREDIENT:
+            return {
+                ...state, // only so we don't creat a deep clone of the object
+                ingredients: {
+                    ...state.ingredients, //for that reason i need another copy
+                    //I use [] Es6 for selec and ovveraid a proprety in this case a ingredit
+                    [action.ingredientName]: state.ingredients[action.ingredientname] - 1
+                }
+
+            };
+        default:
+    }
+
+
+    return state;
 
 
 };
